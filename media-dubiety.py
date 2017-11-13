df8b014062f22f800466df11c557eb56be6d2b7d
zhuyifei1999 <zhuyifei1999@gmail.com>
2017-11-12 19:15:48 -0600

    MOAR code!
    Initial code for SSE -> IRC added. The actual event processing
    logic is not yet done.

--- media-dubiety.py
+++ media-dubiety.py
@@ -15,8 +15,10 @@
 # along with self program.  If not, see <http://www.gnu.org/licenses/>
 #
 
-from __future__ import unicode_literals
+from __future__ import print_function, unicode_literals
 
+import collections
+import fnmatch
 import json
 import threading
 import time
@@ -33,8 +35,8 @@ from pywikibot.comms.eventstreams import EventStreams
 
 with open(os.path.expanduser('~/.ircconf.json'), 'r') as f:
     ircconf = json.load(f)
-
-interrupt = threading.Event()
+with open(os.path.expanduser('~/channels.json'), 'r') as f:
+    channels = json.load(f, object_pairs_hook=collections.OrderedDict)
 
 
 class MediaDubietyIRC(
@@ -42,8 +44,8 @@ class MediaDubietyIRC(
     ib3.connection.SSL,
     ib3.mixins.DisconnectOnError,
     ib3.mixins.PingServer,
-    ib3.mixins.RejoinOnBan,
-    ib3.mixins.RejoinOnKick,
+    # ib3.mixins.RejoinOnBan,
+    # ib3.mixins.RejoinOnKick,
     ib3.nick.Regain,
     ib3.Bot,
     threading.Thread,
@@ -56,10 +58,11 @@ class MediaDubietyIRC(
             nickname=ircconf['nick'],
             realname=ircconf['realname'],
             ident_password=ircconf['password'],
-            channels=ircconf['channels'],
+            channels=channels.values(),
         )
         threading.Thread.__init__(self, name='IRC')
 
+        self.interrupt_event = threading.Event()
         self.reactor.scheduler.execute_every(
             period=1, func=self.check_interrupt)
 
@@ -71,42 +74,70 @@ class MediaDubietyIRC(
         threading.Thread.start(self)
 
     def check_interrupt(self):
-        if interrupt.isSet():
+        if self.interrupt_event.isSet():
+            self.connection.disconnect()
             raise KeyboardInterrupt
+
+    def interrupt(self):
+        self.interrupt_event.set()
+
+    def msg(self, channels, msg):
+        if not self.has_primary_nick():
+            return
+
+        self.connection.privmsg_many(channels, msg)
 
 
 class MediaDubietySSE(threading.Thread):
-    def __init__(self):
+    def __init__(self, irc):
         super(MediaDubietySSE, self).__init__(name='SSE')
+        self.irc = irc
+        self.interrupt_event = threading.Event()
 
     def run(self):
         stream = EventStreams(stream='recentchange')
         for event in stream:
-            if interrupt.isSet():
+            if self.interrupt_event.isSet():
                 raise KeyboardInterrupt
-            # print(event)
+
+            if (event['type'] == 'log' and
+                    event['log_type'] in ['upload', 'block']):
+                EventHandler(event, self.irc).start()
+
+    def interrupt(self):
+        self.interrupt_event.set()
+
+
+class EventHandler(threading.Thread):
+    def __init__(self, event, irc):
+        super(EventHandler, self).__init__(
+            name='Event %(wiki)s.%(id)d' % event)
+        self.daemon = True
+        self.event = event
+        self.irc = irc
+
+    def run(self):
+        privmsg_channels = []
+        for glob, channel in channels.items():
+            if fnmatch.fnmatch(self.event['server_name'], glob):
+                privmsg_channels.append(channel)
+        self.irc.msg(privmsg_channels, self.event['meta']['uri'])
 
 
 def main():
     irc = MediaDubietyIRC(ircconf)
-    sse = MediaDubietySSE()
+    sse = MediaDubietySSE(irc)
     irc.start()
     sse.start()
     try:
-        while irc.isAlive() or sse.isAlive():
-            try:
-                irc.join(0)
-            except RuntimeError:
-                pass
-            try:
-                sse.join(0)
-            except RuntimeError:
-                pass
-
+        while irc.isAlive() and sse.isAlive():
             time.sleep(1)
-    except KeyboardInterrupt:
-        interrupt.set()
-        raise
+    finally:
+        sse.interrupt()
+        irc.interrupt()
+        for thread in threading.enumerate():
+            if thread.daemon:
+                print('Abandoning daemon thread %s' % thread.name)
 
 
 if __name__ == '__main__':
